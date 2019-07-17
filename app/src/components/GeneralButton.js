5542d2bc9554e86bf60b8337ddf8647c9e21e378
Matt Carroll <mwcarroll12@gmail.com>
2019-07-17 21:55:11 +0200

    components and pages added

---
+++ app/src/components/GeneralButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './components/Button.css';
+
+class GeneralButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handlePress = this.handlePress.bind(this);
+    }
+
+    render() {
+        return (
+            <button
+                className={"Button"}
+                block
+                bsSize="large"
+                type="submit"
+                onClick={this.props.handlePress}
+            >
+                {this.props.text}
+            </button>
+        );
+    }
+}
+
+export default GeneralButton;
---
+++ app/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GeneralButton from "./components/GeneralButton";
+
+function App() {
+    return (
+        <div className="App">
+            <GeneralButton/>
+        </div>
+    );
+}
+
+export default App;
