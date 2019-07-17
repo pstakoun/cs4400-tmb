7eecafffc4b7dff01c6ea80d9e3d6fb87cde5f63
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-18 00:37:37 +0200

    passenger landing pages stuff."

--- app/src/pages/PassengerHome.js
+++ app/src/pages/PassengerHome.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import MenuItem from '../components/MenuItem';
+import GeneralButton from '../components/GeneralButton.js';
+import './PassHome.css';
+
 
 function PassengerHome() {
-  return (
-    <div className="PassengerHome">
-      <MenuItem />
-    </div>
-  );
-}
+
+    return (
+       
+        <div className = "Wrapper">  
+            
+            <div className = "Cards">  
+            <div className={"ButtonWrapper"}>
+               
+                <GeneralButton text = {"Leave Review"}/>
+                <GeneralButton text = {"View Review"}/>
+                <GeneralButton text = {"Buy Card"}/>
+                <GeneralButton text = {"Go on Trip"}/>
+                <GeneralButton text = {"View Trips"}/>
+                <GeneralButton text = {"Edit"}/>
+
+         </div>
+
+         </div>
+</div>
+    );
 
 export default PassengerHome;
