5bfc9ef9e52474b6302b249fd7d2acee539e66c0
Matt Carroll <mwcarroll12@gmail.com>
2019-07-18 04:11:24 +0200

    finished up review

--- app/src/pages/LeaveReview.js
+++ app/src/pages/LeaveReview.js
@@ -12,30 +12,47 @@ class LeaveReview extends React.Component {
     super(props);
     this.handleRateShopping = this.handleRateShopping.bind(this);
     this.handleRateSpeed = this.handleRateSpeed.bind(this);
+    this.handleCommentChange = this.handleCommentChange.bind(this);
     this.state = {
       options: [],
       defaultOption: '',
+      shoppingRating: 0,
+      speedRating: 0,
+      comment: ''
     };
   }
 
   handleRateShopping(rating) {
-    console.log(`Shopping Rating is: ${rating}`);
+    console.log(`Shopping Rating is: ` + this.state.shoppingRating);
+    this.setState({shoppingRating: rating});
   }
 
   handleRateSpeed(rating) {
     console.log(`Connection Speed Rating is: ${rating}`);
+    this.setState({speedRating: rating});
+  }
+
+  handleCommentChange(event) {
+    this.setState({comment: event.target.value});
   }
 
   render() {
     return (
       <div className="Wrapper">
         <div className="LeaveReview">
-          <Dropdown options={this.state.options} onChange={this._onSelect} value={this.state.defaultOption} placeholder="Select a Station" />
-          <ReviewStars text="Shopping" name="shopping" handleRate={this.handleRateShopping} />
-          <ReviewStars text="Connection Speed" name="speed" handleRate={this.handleRateSpeed} />
+          <Dropdown
+            options={this.state.options}
+            onChange={this._onSelect}
+            value={this.state.defaultOption}
+            placeholder="Select a Station"
+          />
+          <ReviewStars text="Shopping" rating={this.state.shoppingRating} name="shopping" handleRate={this.handleRateShopping} />
+          <ReviewStars text="Connection Speed" rating={this.state.speedRating} name="speed" handleRate={this.handleRateSpeed} />
           <textarea
             rows="5"
             placeholder="Comment here..."
+            onChange={this.handleCommentChange}
+            value={this.state.comment}
           />
           <div className="ButtonWrapper">
             <GeneralButton text="Submit Review" />
