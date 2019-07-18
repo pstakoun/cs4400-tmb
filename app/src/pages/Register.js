bdc1829680d61602873311f3f5b0640919c3583d
Matt Carroll <mwcarroll12@gmail.com>
2019-07-18 03:49:34 +0200

    fixed form layout for register

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -5,17 +5,74 @@ import GeneralButton from '../components/GeneralButton.js';
 import TextField from '../components/TextField.js';
 
 class Register extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      FirstName: "",
+      MiddleInitial: "",
+      LastName: "",
+      Email: "",
+      UserID: "",
+      Password: "",
+      PasswordCheck: ""
+    }
+    this.fNameChange = this.fNameChange.bind(this);
+    this.MIChange = this.MIChange.bind(this);
+    this.lNameChange = this.lNameChange.bind(this);
+    this.emailChange = this.emailChange.bind(this);
+    this.userIDChange = this.userIDChange.bind(this);
+    this.passwordChange = this.passwordChange.bind(this);
+    this.passwordCheckChange = this.passwordCheckChange.bind(this);
+  }
+
+  fNameChange(event) {
+    this.setState({ FirstName: event.target.value});
+  }
+
+  MIChange(event) {
+    this.setState({ MiddleInitial: event.target.value});
+  }
+
+  lNameChange(event) {
+    this.setState({ LastName: event.target.value});
+  }
+
+  emailChange(event) {
+    this.setState({ Email: event.target.value});
+  }
+
+  userIDChange(event) {
+    this.setState({ UserID: event.target.value});
+  }
+
+  passwordChange(event) {
+    this.setState({ Password: event.target.value});
+  }
+
+  passwordCheckChange(event) {
+    this.setState({ PasswordCheck: event.target.value});
+  }
+
+  checkPass() {
+    console.log(this.state.Password + " | " + this.state.PasswordCheck)
+    if (this.state.Password === this.state.PasswordCheck) {
+      console.log("Passwords match");
+    } else {
+      console.log("Passwords do not match");
+    }
+  }
+
   render() {
     return (
       <div className="Wrapper">
         <div className="Register">
-          <TextField text="First Name" type="text" />
-          <TextField text="Middle Initial" type="text" />
-          <TextField text="Last Name" type="text" />
-          <TextField text="Email" type="text" />
-          <TextField text="User ID (unique)" type="text" />
-          <TextField text="Password" type="password" />
-          <TextField text="Password (again)" type="password" />
+          <TextField text="First Name" type="text" handleChange={this.fNameChange}/>
+          <TextField text="Middle Initial" type="text" handleChange={this.MIChange}/>
+          <TextField text="Last Name" type="text" handleChange={this.lNameChange}/>
+          <TextField text="Email" type="text" handleChange={this.emailChange}/>
+          <TextField text="User ID (unique)" type="text"handleChange={this.userIDChange} />
+          <TextField text="Password" type="password" handleChange={this.passwordChange}/>
+          <TextField text="Password (again)" type="password" handleChange={this.passwordCheckChange} onChange={this.checkPass()}/>
           <div className="ButtonWrapper">
             <Link to="/">
               <GeneralButton text="I already have an account" />
