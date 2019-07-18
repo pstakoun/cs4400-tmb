b758a867e949ff03b6bc2e739f96867e08abb5c1
Matt Carroll <mwcarroll12@gmail.com>
2019-07-18 02:37:17 +0200

    Leave Review added

--- app/src/pages/LeaveReview.js
+++ app/src/pages/LeaveReview.js
@@ -4,19 +4,43 @@ import { Link } from 'react-router-dom';
 import GeneralButton from '../components/GeneralButton.js';
 import TextField from '../components/TextField.js';
 import ReviewStars from '../components/ReviewStars.js';
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
 
 class LeaveReview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRateShopping = this.handleRateShopping.bind(this);
+    this.handleRateSpeed = this.handleRateSpeed.bind(this);
+    this.state = {
+      "options" : [],
+      "defaultOption" : ""
+    }
+  }
+
+  handleRateShopping(rating) {
+    console.log("Shopping Rating is: " + rating)
+  }
+
+  handleRateSpeed(rating) {
+    console.log("Connection Speed Rating is: " + rating)
+  }
+
   render()
   {
     return (
       <div className="Wrapper">
         <div className="LeaveReview">
-          <ReviewStars text={"Shopping"}/>
-          <ReviewStars text={"Connection Speed"}/>
+          <Dropdown options={this.state.options} onChange={this._onSelect} value={this.state.defaultOption} placeholder="Select a Station" />
+          <ReviewStars text={"Shopping"} name={"shopping"} handleRate={this.handleRateShopping}/>
+          <ReviewStars text={"Connection Speed"} name={"speed"} handleRate={this.handleRateSpeed}/>
           <textarea
             rows="5"
-
+            placeholder={"Comment here..."}
           />
+          <div className={"ButtonWrapper"}>
+            <GeneralButton text={"Submit Review"}/>
+          </div>
         </div>
       </div>
     );
