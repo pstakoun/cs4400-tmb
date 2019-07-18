2bed26949c53200362094494fc06400b48f6bf85
Matt Carroll <mwcarroll12@gmail.com>
2019-07-19 00:12:44 +0200

    add buy card functionality

--- app/src/pages/BuyCard.js
+++ app/src/pages/BuyCard.js
@@ -5,29 +5,77 @@ import './Home.css';
 import './BuyCard.css';
 
 
-function BuyCard() {
-  return (
-    <div className="Wrapper">
-      <div className="Cards">
-        <div className="ButtonWrapper">
-          <header>
-            <h3>Buy Trip Card</h3>
-          </header>
+class BuyCard extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateTime: '',
+      futureDateTime: '',
+    }
+  }
 
-          <b className="b">Pick Card Type:</b>
-          <GeneralButton text="T-mes" />
-          <GeneralButton text="T-10" />
-          <GeneralButton text="T-50/30" />
-          <GeneralButton text="T-jove" />
-        </div>
-        <div className="ButtonWrapper">
-          <Link to="/">
-            <GeneralButton text="Main Menu" />
-          </Link>
+  calculateDateTimes(days) {
+    var today = new Date();
+    var future = new Date();
+    future.setDate(future.getDate() + days);
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var futureDate = future.getFullYear()+'-'+(future.getMonth()+1)+'-'+future.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    this.state.dateTime = date+' '+time;
+    this.state.futureDateTime = futureDate+' '+time;
+    console.log(this.state.dateTime);
+    console.log(this.state.futureDateTime);
+  }
+
+  handlePurchase(name, uses, daysRemaining) {
+    if (daysRemaining != null) {
+      this.calculateDateTimes(daysRemaining);
+    }
+    fetch('/api/cards/purchase', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      body: JSON.stringify({
+        type: name,
+        purchaseDateAndTime: this.state.dateTime,
+        usesLeft: uses,
+        expirationDate: this.state.futureDateTime,
+      }),
+    }).then(res => res.json()).then((data) => {
+      if (data.success) {
+        alert(name + " purchased");
+      } else {
+        alert(data.message);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="Wrapper">
+        <div className="Cards">
+          <div className="ButtonWrapper">
+            <header>
+              <h3>Buy Trip Card</h3>
+            </header>
+
+            <b className="b">Pick Card Type:</b>
+            <GeneralButton text="T-mes" handlePress={this.handlePurchase.bind(this, "T-mes", null, 30)}/>
+            <GeneralButton text="T-10" handlePress={this.handlePurchase.bind(this, "T-10", 10, null)}/>
+            <GeneralButton text="T-50/30" handlePress={this.handlePurchase.bind(this, "T-50/30", 50, 30)}/>
+            <GeneralButton text="T-jove" handlePress={this.handlePurchase.bind(this, "T-jove", null, 90)}/>
+          </div>
+          <div className="ButtonWrapper">
+            <Link to="/">
+              <GeneralButton text="Main Menu" />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default BuyCard;
--- routes/cards.js
+++ routes/cards.js
@@ -1,20 +1,39 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const { connection } = require('../services');
 
 const router = express.Router();
 
 /* GET cards */
-router.post('/buy', (req, res) => {
+router.get('/', (req, res) => {
+  res.status(200).json({});
+});
+
+/* GET cards */
+router.post('/purchase', (req, res) => {
   const {
-    userID,
     type,
     purchaseDateAndTime,
     usesLeft,
     expirationDate,
   } = req.body;
 
-  res.status(200).json({ message: 'Success' });
+  const card = {
+    user_ID: req.session.user.ID,
+    type: type,
+    purchase_date_time: purchaseDateAndTime,
+    uses_left: usesLeft,
+    expiration_date: expirationDate,
+  };
+
+  connection.query('INSERT INTO Card SET ?', card, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'An error ocurred' });
+    }
+    res.status(200).json({ success: true, message: 'Success' });
+  });
 });
 
 module.exports = router;
