e35aa4396ac316e55d379356e847f14434db5df7
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-18 04:03:52 +0200

    fucking lint/.

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => res.status(200).json({}));
 
 /* Register user */
 router.post('/register', (req, res) => {
-  var user = {
+  const user = {
     firstName,
     middleInitial,
     lastName,
@@ -33,32 +33,32 @@ router.post('/register', (req, res) => {
   if (user.password != user.confirmPassword) {
     alert("Passwords don't match");
     res.send({
-      "code":500,
-      "failed":"Passwords don't match"
-    })
-  } else if (length(user.password) < 8) { //TODO MOVE THIS TO SQL!
-    alert("Password must be longer then 8 characters");
-    res.send({
-      "code": 500,
-      "failed":"Passwords too short"
-    })
-  } else {
-  connection.query('INSERT INTO User SET ?', user, function (error, results, fields) {
-    if (error) {
-      console.log("error ocurred",error);
-      res.send({
-        "code":500,
-        "failed":"error ocurred"
-      })
-    }else{
-      console.log('The solution is: ', results);
-      res.send({
-        "code":200,
-        "success": "user registered sucessfully"
-          });
-    }
+      code: 500,
+      failed: "Passwords don't match",
     });
-  /*connection.query('TODO', (error, results, fields) => {
+  } else if (length(user.password) < 8) { // TODO MOVE THIS TO SQL!
+    alert('Password must be longer then 8 characters');
+    res.send({
+      code: 500,
+      failed: 'Passwords too short',
+    });
+  } else {
+    connection.query('INSERT INTO User SET ?', user, (error, results, fields) => {
+      if (error) {
+        console.log('error ocurred', error);
+        res.send({
+          code: 500,
+          failed: 'error ocurred',
+        });
+      } else {
+        console.log('The solution is: ', results);
+        res.send({
+          code: 200,
+          success: 'user registered sucessfully',
+        });
+      }
+    });
+  /* connection.query('TODO', (error, results, fields) => {
     if (error) {
       throw error;
       // return res.sendStatus(500);
@@ -68,8 +68,8 @@ router.post('/register', (req, res) => {
     }
     // TODO insert user
     return res.sendStatus(200);
-  });*/
-}
+  }); */
+  }
 });
 
 /* Log in user */
@@ -80,39 +80,37 @@ router.post('/login', (req, res) => {
   } = req.body;
   // TODO confirm user in db
 
-  connection.query('SELECT * FROM User WHERE ID = ?',[userID], function (error, results, fields) {
+  connection.query('SELECT * FROM User WHERE ID = ?', [userID], (error, results, fields) => {
     if (error) {
       // console.log("error ocurred",error);
       res.send({
-        "code":400,
-        "failed":"error ocurred"
-      })
-    }else{
+        code: 400,
+        failed: 'error ocurred',
+      });
+    } else {
       // console.log('The solution is: ', results);
-      if(results.length >0){
-        if(results[0].password == password){
+      if (results.length > 0) {
+        if (results[0].password == password) {
           res.send({
-            "code":200,
-            "success":"login sucessfull"
-              });
-        }
-        else{
+            code: 200,
+            success: 'login sucessfull',
+          });
+        } else {
           res.send({
-            "code":204,
-            "success":"Email and password does not match"
-              });
+            code: 204,
+            success: 'Email and password does not match',
+          });
         }
-      }
-      else{
+      } else {
         res.send({
-          "code":204,
-          "success":"Email does not exits"
-            });
+          code: 204,
+          success: 'Email does not exits',
+        });
       }
     }
-    });
+  });
 
-    //TODO route to admin home or not based on if admin or not
+  // TODO route to admin home or not based on if admin or not
   const user = {
     id: userID,
     admin: false,
