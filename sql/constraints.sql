b3242b10268a22aa2082ee057d7f16d49c6062c1
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-19 22:49:43 +0200

    fixed stuff/

--- sql/constraints.sql
+++ sql/constraints.sql
@@ -1,23 +1,47 @@
 USE TMB;
+
+#Constraints for registration.  (email must be null for admins)
 ALTER TABLE User MODIFY COLUMN first_name VARCHAR(255) NOT NULL;
 ALTER TABLE User ADD CONSTRAINT first_name CHECK (Length(first_name) != 0);
 ALTER TABLE User MODIFY COLUMN last_name VARCHAR(255) NOT NULL;
 ALTER TABLE User ADD CONSTRAINT last_name CHECK (Length(last_name) != 0);
-ALTER TABLE User ADD CONSTRAINT email CHECK (Length(email) != 0);
-
-
+ALTER TABLE User ADD CONSTRAINT passenger_email CHECK (Length(passenger_email) != 0);
 ALTER TABLE User ADD CONSTRAINT pass_length CHECK (Length(password) > 7);
 
 
 
-
-
+#Constraints for adding reviews. 
 ALTER TABLE Review MODIFY COLUMN rid INT NOT NULL AUTO_INCREMENT UNIQUE;
 ALTER TABLE Review ADD CONSTRAINT connection_speed CHECK(connection_speed > 0 AND connection_speed <= 5);
 ALTER TABLE Review ADD CONSTRAINT shopping CHECK(shopping > 0 AND shopping <= 5);
-
-
-
-#ALTER TABLE Review MODIFY COLUMN approval_status SET DEFAULT 'Pending';
 ALTER TABLE Review ALTER approval_status SET DEFAULT 'Pending';
+
+
+
+#  Constraints for deleting users. cascading on Review, Card, and Trip
+ALTER TABLE Review
+   ADD CONSTRAINT passenger_ID_cascade
+   FOREIGN KEY (passenger_ID) REFERENCES User(ID) ON DELETE CASCADE;
+ALTER TABLE Card
+   ADD CONSTRAINT user_card_cascades
+   FOREIGN KEY (user_ID) REFERENCES User(ID) ON DELETE CASCADE;
+
+ALTER TABLE TRIP
+   ADD CONSTRAINT user_trip_cascade
+   FOREIGN KEY (user_ID) REFERENCES Card(user_ID) ON DELETE CASCADE;
+
+
+
+
+# TODO!
+#drop foriegn key for admin
+#add cascade 
+#delete admin approval id in review table. cascade to null. change status to pending.
+#todo fix error messages for all constraints.
+#continue adding and double checking constraints as we go.
+
+
+
+
+
 
