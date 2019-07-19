7f1a963dd5dcf6f76a30f2eccc04c4608a9dd6b7
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-19 14:27:47 +0200

    fixed

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,17 @@ router.get('/', (req, res) => {
   res.status(200).json({});
 });
 
+/* GET current user pass */
+router.get('/pass', (req, res) => {
+  connection.query('SELECT password FROM User WHERE ID = ?', [req.session.user.ID], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'An error ocurred' });
+    }
+    res.status(200).json(result);
+  });
+});
+
 /* Register user */
 router.post('/register', (req, res) => {
   const {
@@ -47,6 +58,55 @@ router.post('/register', (req, res) => {
   });
 });
 
+/* Update Profile */
+router.put('/update', (req, res) => {
+  const {
+    firstName,
+    middleInitial,
+    lastName,
+    email,
+    userID,
+    password,
+    confirmPassword,
+  } = req.body;
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: 'Passwords do not match' });
+  }
+  if (password.length < 8) {
+    return res.status(400).json({ message: 'Password too short' });
+  }
+  const user = {
+    ID: userID,
+    first_name: firstName,
+    minit: middleInitial,
+    last_name: lastName,
+    password, // TODO hash password
+    passenger_email: email,
+  };
+
+  connection.query('UPDATE User SET ? WHERE ID = ?', [review, req.session.user.ID], (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'An error ocurred' });
+    }
+    res.status(200).json({ success: true, message: 'Success' });
+  });
+});
+
+
+/* Delete User */
+router.delete('/delete', (req, res) => {
+  connection.query('DELETE FROM User WHERE ID = ?', [req.session.user.ID], (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'An error ocurred' });
+    }
+    req.session.user = null;
+    res.status(200).json({ success: true, message: 'Success' });
+  });
+});
+
+
 /* Log in user */
 router.post('/login', (req, res) => {
   const {
@@ -74,7 +134,7 @@ router.post('/login', (req, res) => {
 
     connection.query('SELECT * FROM Admin WHERE ID = ?', [userID], (err1, result1) => {
       if (err1) {
-        console.log(err1);
+        console.log(err1.message);
         return res.status(500).json({ message: 'An error ocurred' });
       }
 
