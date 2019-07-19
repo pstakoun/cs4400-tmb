0e702cb090572a93b0c35fb8c4b97ae44ecf1a2f
Peter Stakoun <peter@stakoun.com>
2019-07-19 11:30:57 +0200

    Update API results

--- routes/lines.js
+++ routes/lines.js
@@ -6,12 +6,12 @@ const router = express.Router();
 
 /* GET lines */
 router.get('/', (req, res) => {
-  connection.query('SELECT name FROM Line', (err1, result1) => {
-    if (err1) {
-      console.log(err1);
+  connection.query('SELECT name FROM Line', (err, result) => {
+    if (err) {
+      console.log(err);
       return res.status(500).json({ message: 'An error ocurred' });
     }
-    res.status(200).json(result1);
+    res.status(200).json({ lines: result });
   });
 });
 
