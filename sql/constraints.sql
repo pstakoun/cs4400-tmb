7808017c55e2e962fe62d96de8cb69271a30861d
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-19 03:06:46 +0200

    constraints fixed AGIAN

--- sql/constraints.sql
+++ sql/constraints.sql
@@ -1,3 +1,15 @@
 USE TMB;
-BEGIN;
 ALTER TABLE User ADD CONSTRAINT pass_length CHECK (Length(password) > 7);
+
+
+/* TODO add default pending status for insert review */
+
+ALTER TABLE Review MODIFY COLUMN rid INT NOT NULL AUTO_INCREMENT UNIQUE;
+
+
+#ALTER TABLE Review MODIFY COLUMN approval_status SET DEFAULT 'Pending';
+
+
+
+ALTER TABLE Review
+ALTER approval_status SET DEFAULT 'Pending';
