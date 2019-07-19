f45e3724401baa89d232bc4cd844e8ef5877bcd6
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-19 09:49:50 +0200

    fixed constraints.

--- sql/constraints.sql
+++ sql/constraints.sql
@@ -3,7 +3,6 @@ ALTER TABLE User MODIFY COLUMN first_name VARCHAR(255) NOT NULL;
 ALTER TABLE User ADD CONSTRAINT first_name CHECK (Length(first_name) != 0);
 ALTER TABLE User MODIFY COLUMN last_name VARCHAR(255) NOT NULL;
 ALTER TABLE User ADD CONSTRAINT last_name CHECK (Length(last_name) != 0);
-ALTER TABLE User MODIFY COLUMN email VARCHAR(255) NOT NULL;
 ALTER TABLE User ADD CONSTRAINT email CHECK (Length(email) != 0);
 
 
@@ -21,5 +20,4 @@ ALTER TABLE Review ADD CONSTRAINT shopping CHECK(shopping > 0 AND shopping <= 5)
 
 #ALTER TABLE Review MODIFY COLUMN approval_status SET DEFAULT 'Pending';
 ALTER TABLE Review ALTER approval_status SET DEFAULT 'Pending';
-
 
