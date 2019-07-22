07c8cbb5556be3cf9c740e953cce48e7441aec47
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-22 16:09:37 +0200

    fixed data on card data.

--- sql/sample-data.sql
+++ sql/sample-data.sql
@@ -13,9 +13,9 @@ INSERT INTO Admin VALUES('wwhite12');
 INSERT INTO Admin VALUES('peter-admin');
 #fix these warnings... on card
 INSERT INTO Card VALUES ('chall68',	'T-mes',	'2019-06-08 10:27:00',	NULL,
-'2019-07-08 10:27:00');
+'2019-07-08');
 INSERT INTO Card VALUES ('peter',	'T-mes',	'2019-06-08 10:27:00',	NULL,
-'2019-07-08 10:27:00');
+'2019-07-08');
 INSERT INTO Station VALUES('Catalunya',	'open',	'Catalonia',	'08002 Barcelona', '08002', 'Barcelona');
 INSERT INTO Station VALUES('Crapalunya',	'closed',	'Catalonia',	'80080 Barcelona', '80080', 'Barcelona');
 INSERT INTO Station VALUES('Poopalunya',	'open',	'Catalonia',	'80085 Barcelona', '80085', 'Barcelona');
