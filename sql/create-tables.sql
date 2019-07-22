89d1dd3ff2f015ae709722967b0205a737c259c2
Andreas Paljug <andreaspaljug@gmail.com>
2019-07-22 19:01:30 +0200

    pass email.

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE User
     first_name varchar(255) NOT NULL,
     minit char(1),
     last_name varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
+    password varchar(255),
     passenger_email varchar(255) NOT NULL,
     PRIMARY KEY (ID)
 );
