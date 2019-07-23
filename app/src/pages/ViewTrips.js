944716147734db327f8d6fce7edbbf7c7fc9212f
Matt Carroll <mwcarroll12@gmail.com>
2019-07-23 05:39:27 +0200

    fixed viewtrips

--- app/src/pages/ViewTrips.js
+++ app/src/pages/ViewTrips.js
@@ -70,7 +70,7 @@ class ViewTrips extends React.Component {
                   field: 'from_station_name',
                   render: rowData => (
                     <Link to={{
-                      pathname: '/stationInfo',
+                      pathname: this.props.user.admin ? '/stationInfoAdmin' : '/stationInfo',
                       state: {
                         stationName: rowData.from_station_name,
                       },
@@ -83,24 +83,9 @@ class ViewTrips extends React.Component {
                 {
                   title: 'To',
                   field: 'to_station_name',
-
-                  //TODO the commented section is what should run if in admin
-                  /*
                   render: rowData => (
                     <Link to={{
-                      pathname: '/stationInfoAdmin',
-                      state: {
-                        stationName: rowData.station_name,
-                      },
-                    }}
-                    >
-                      {rowData.station_name}
-                    </Link>
-                  ),
-                   */
-                  render: rowData => (
-                    <Link to={{
-                      pathname: '/stationInfo',
+                      pathname: this.props.user.admin ? '/stationInfoAdmin' : '/stationInfo',
                       state: {
                         stationName: rowData.to_station_name,
                       },
